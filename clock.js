3a0737ebe0500a42eb16d0a0fc98b580cd4d6fa3
Amed Moreno <32321092+danewdesign@users.noreply.github.com>
2019-09-14 17:05:22 -0400

    -fixed several bugs and added layout for mobile
    -once alarm set, if "set alarm" was clicked again, "alarm off" would not stop alarm sound
    	-disabled button on click to resolve
    -once "alarm off" was clicked, if you immediately hit "set alarm" again, previous selected alarm time
    would display and alarm would reactivate
    	-disabled "set alarm" button on click to resolve
    -alarm would stop sounding once current time minutes surpassed selected alarm time value 
    (i.e. alarm set 12:00pm, once current time gets to 12:01pm, alarm stops)
    	-set conditional from myAlarm == currentTime to currentTime >= myAlarm to resolve
    -added responsive styling/layout for mobile

--- clock.js
+++ clock.js
@@ -44,57 +44,66 @@ setInterval(time, 1000);
 
 /**************************** THE Alarm *******************************/
 // required globally available variables
-var setAlarm = $("#submit"), alarmHour, alarmMinutes, alarmMeridian, checkCompare;
+var setAlarm = $("#submit"),
+  alarmOn = false,
+  alarmHour, alarmMinutes, alarmMeridian,
+  checkCompare, alarmDisplay, myAlarm, currentTime;
+
 var beepBeep = new Audio("sounds/alarm.wav");
 
 // retrieve selected values
-  $("#hours").change(function() {
-    alarmHour = $(this).val();
-  });
-  $("#minutes").change(function() {
-    alarmMinutes = $(this).val();
-  });
-  $("#meridian").change(function() {
-    alarmMeridian = $(this).val();
-  });
+$("#hours").change(function() {
+  alarmHour = $(this).val();
+});
+$("#minutes").change(function() {
+  alarmMinutes = $(this).val();
+});
+$("#meridian").change(function() {
+  alarmMeridian = $(this).val();
+  alarmOn = true;
+});
 
 // display selected alarm time in heading element
 setAlarm.click(function() {
-  var alarmDisplay = $(".my-alarm");
-  alarmDisplay.html("<h4>" + "Your alarm is set for " + "<br>" + alarmHour + ":" + alarmMinutes + " " + alarmMeridian + "</h4>");
-  $(".my-alarm").slideDown(1000);
-  $(".alarm-box").slideUp(1000);
+  if (alarmOn == true) {
+    alarmDisplay = $(".my-alarm");
+    alarmDisplay.html("<h4>" + "Your alarm is set for " + "<br>" + alarmHour + ":" + alarmMinutes + " " + alarmMeridian + "</h4>");
+    $(".my-alarm").slideDown(1000);
+    $(".alarm-box").slideUp(1000);
 
-  // compare selected alarm time with current time
-  checkCompare = setInterval(function() {
+    // compare selected alarm time with current time
+    checkCompare = setInterval(function() {
 
-    var timeCompare = new Date();
-    var hoursCompare = timeCompare.getHours();
-    var minutesCompare = timeCompare.getMinutes();
-    var meridianCompare = "AM";
+      var timeCompare = new Date();
+      var hoursCompare = timeCompare.getHours();
+      var minutesCompare = timeCompare.getMinutes();
+      var meridianCompare = "AM";
 
-    if (hoursCompare >= 12) {
-      meridianCompare = "PM";
-      if (hoursCompare > 12) {
-        hoursCompare = hoursCompare - 12;
+      if (hoursCompare >= 12) {
+        meridianCompare = "PM";
+        if (hoursCompare > 12) {
+          hoursCompare = hoursCompare - 12;
+        }
+        if (hoursCompare < 10) {
+          hoursCompare = "0" + hoursCompare;
+        }
       }
-      if (hoursCompare < 10) {
-        hoursCompare = "0" + hoursCompare;
+
+      if (minutesCompare < 10) {
+        minutesCompare = "0" + minutesCompare;
       }
-    }
 
-    if (minutesCompare < 10) {
-      minutesCompare = "0" + minutesCompare;
-    }
+      currentTime = hoursCompare + ":" + minutesCompare + " " + meridianCompare;
 
-    var currentTime = hoursCompare + ":" + minutesCompare + " " + meridianCompare;
+      myAlarm = alarmHour + ":" + alarmMinutes + " " + alarmMeridian;
 
-    var myAlarm = alarmHour + ":" + alarmMinutes + " " + alarmMeridian;
+      if (currentTime >= myAlarm) {
+        beepBeep.play();
+      }
+    }, 1000);
+    setAlarm.prop('disabled', true);
+  }
 
-    if (myAlarm == currentTime) {
-      beepBeep.play();
-    }
-  }, 1000);
 });
 
 // Reset alarm
@@ -106,4 +115,9 @@ $("#clear").click(function() {
   $("#hours").prop("selectedIndex", 0);
   $("#minutes").prop("selectedIndex", 0);
   $("#meridian").prop("selectedIndex", 0);
+  setAlarm.prop('disabled', false);
+  alarmHour = undefined;
+  alarmMinutes = undefined;
+  alarmMeridian = undefined;
+  alarmOn = false;
 });
